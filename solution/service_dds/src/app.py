9f4c8c661600b0c72a94066a0fcf90d261cb0906
jungcholshin <jungcholshin@gmail.com>
2026-04-01 13:40:30 +0900

    Update app.py

--- solution/service_dds/src/app.py
+++ solution/service_dds/src/app.py
@@ -37,3 +37,4 @@ if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0', use_reloader=False)
     app.run(debug=True, host='0.0.0.0', use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', use_reloader=False)

8760d2c0277f4fb58558e7c314de9089b8bf98f2
jungcholshin <jungcholshin@gmail.com>
2026-04-01 12:39:19 +0900

    Update kafka_connectors.py

--- solution/service_dds/src/lib/kafka_connect/kafka_connectors.py
+++ solution/service_dds/src/lib/kafka_connect/kafka_connectors.py
@@ -1,11 +1,34 @@
 import json
 from typing import Dict, Optional
 
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, Producer
 
 
 def error_callback(err):
     print(f'Something went wrong: {err}')
+
+
+class KafkaProducer:
+    def __init__(self, host: str, port: int, user: str, password: str, topic: str, cert_path: str) -> None:
+        params = {
+            'bootstrap.servers': f'{host}:{port}',
+            'security.protocol': 'SASL_SSL',
+            'ssl.ca.location': cert_path,
+            'sasl.mechanism': 'SCRAM-SHA-512',
+            'sasl.username': user,
+            'sasl.password': password,
+            'error_cb': error_callback,
+        }
+
+        self.topic = topic
+        self.p = Producer(params)
+
+    def produce(self, payload: Dict) -> None:
+        self.p.produce(self.topic, json.dumps(payload))
+        self.p.flush(10)
+
+    def close(self) -> None:
+        self.p.flush(10)
 
 
 class KafkaConsumer:
@@ -30,11 +53,28 @@ class KafkaConsumer:
             'auto.offset.reset': 'earliest',
             'enable.auto.commit': False,
             'error_cb': error_callback,
-            'client.id': 'cdm-service-client',
+            'client.id': 'dds-service-client',
         }
 
         self.topic = topic
         self.c = Consumer(params)
+        self.c.subscribe([topic])
+
+    def consume(self, timeout: float = 3.0) -> Optional[Dict]:
+        msg = self.c.poll(timeout=timeout)
+        if not msg:
+            return None
+        if msg.error():
+            raise Exception(msg.error())
+
+        val = msg.value().decode('utf-8')
+        return json.loads(val)
+
+    def commit(self) -> None:
+        self.c.commit(asynchronous=False)
+
+    def close(self) -> None:
+        self.c.close()        self.c = Consumer(params)
         self.c.subscribe([topic])
 
     def consume(self, timeout: float = 3.0) -> Optional[Dict]:

58e1c12689857b47af90f9e39f028aea87d2e4ba
jungcholshin <jungcholshin@gmail.com>
2026-04-01 13:32:16 +0900

    Update app_config.py

--- solution/service_cdm/src/app_config.py
+++ solution/service_cdm/src/app_config.py
@@ -1,6 +1,6 @@
 import os
 
-from lib.kafka_connect import KafkaConsumer, KafkaProducer
+from lib.kafka_connect import KafkaConsumer
 from lib.pg import PgConnect
 
 
@@ -8,32 +8,19 @@ class AppConfig:
     CERTIFICATE_PATH = '/crt/YandexInternalRootCA.crt'
 
     def __init__(self) -> None:
-
         self.kafka_host = str(os.getenv('KAFKA_HOST'))
         self.kafka_port = int(str(os.getenv('KAFKA_PORT')))
+
         self.kafka_consumer_username = str(os.getenv('KAFKA_CONSUMER_USERNAME'))
         self.kafka_consumer_password = str(os.getenv('KAFKA_CONSUMER_PASSWORD'))
         self.kafka_consumer_group = str(os.getenv('KAFKA_CONSUMER_GROUP'))
         self.kafka_consumer_topic = str(os.getenv('KAFKA_SOURCE_TOPIC'))
-        self.kafka_producer_username = str(os.getenv('KAFKA_CONSUMER_USERNAME'))
-        self.kafka_producer_password = str(os.getenv('KAFKA_CONSUMER_PASSWORD'))
-        self.kafka_producer_topic = str(os.getenv('KAFKA_DESTINATION_TOPIC'))
 
         self.pg_warehouse_host = str(os.getenv('PG_WAREHOUSE_HOST'))
         self.pg_warehouse_port = int(str(os.getenv('PG_WAREHOUSE_PORT')))
         self.pg_warehouse_dbname = str(os.getenv('PG_WAREHOUSE_DBNAME'))
         self.pg_warehouse_user = str(os.getenv('PG_WAREHOUSE_USER'))
         self.pg_warehouse_password = str(os.getenv('PG_WAREHOUSE_PASSWORD'))
-
-    def kafka_producer(self):
-        return KafkaProducer(
-            self.kafka_host,
-            self.kafka_port,
-            self.kafka_producer_username,
-            self.kafka_producer_password,
-            self.kafka_producer_topic,
-            self.CERTIFICATE_PATH
-        )
 
     def kafka_consumer(self):
         return KafkaConsumer(

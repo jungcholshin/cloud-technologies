6b6ae0a0683e22615a38193518341b8d2792f3c8
jungcholshin <jungcholshin@gmail.com>
2026-04-01 12:17:00 +0900

    app.py

--- solution/service_dds/src/app.py
+++ solution/service_dds/src/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 from app_config import AppConfig
 from dds_loader.dds_message_processor_job import DdsMessageProcessor
+from dds_loader.repository.dds_repository import DdsRepository
 
 app = Flask(__name__)
 
@@ -19,12 +20,19 @@ def hello_world():
 if __name__ == '__main__':
     app.logger.setLevel(logging.DEBUG)
 
+    repository = DdsRepository(config.pg_warehouse_db())
+    consumer = config.kafka_consumer()
+    producer = config.kafka_producer()
+
     proc = DdsMessageProcessor(
-        app.logger
+        logger=app.logger,
+        repository=repository,
+        consumer=consumer,
+        producer=producer,
     )
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=proc.run, trigger="interval", seconds=25)
+    scheduler.add_job(func=proc.run, trigger='interval', seconds=25)
     scheduler.start()
 
     app.run(debug=True, host='0.0.0.0', use_reloader=False)

51f94addadab3d013d486ba7f3b4783b1802f097
jungcholshin <jungcholshin@gmail.com>
2026-04-01 14:07:00 +0900

    Update dds_repository.py

--- solution/service_dds/src/dds_loader/repository/dds_repository.py
+++ solution/service_dds/src/dds_loader/repository/dds_repository.py
@@ -1,6 +1,27 @@
+import uuid
+from datetime import datetime
+
 from lib.pg import PgConnect
 
 
 class DdsRepository:
     def __init__(self, db: PgConnect) -> None:
         self._db = db
+
+    def insert_h_user(self, user_id: str):
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO dds.h_user (h_user_pk, user_id, load_dt, load_src)
+                    VALUES (%s, %s, %s, %s)
+                    ON CONFLICT DO NOTHING;
+                """, (uuid.uuid4(), user_id, datetime.utcnow(), 'kafka'))
+
+    def insert_h_order(self, order_id: str, order_dt: datetime):
+        with self._db.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO dds.h_order (h_order_pk, order_id, order_dt, load_dt, load_src)
+                    VALUES (%s, %s, %s, %s, %s)
+                    ON CONFLICT DO NOTHING;
+                """, (uuid.uuid4(), order_id, order_dt, datetime.utcnow(), 'kafka'))
